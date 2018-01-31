2527fe492eae3a436edf61807c144dadccdcb5c6
Robert Mulder <mulder@creativeid.nl>
2018-01-31 21:39:18 +0100

    Added extension methods for ILoggingBuilder to register the log4net logger

--- src/log4net.Extensions.AspNetCore/log4netExtensions.cs
+++ src/log4net.Extensions.AspNetCore/log4netExtensions.cs
@@ -10,7 +10,40 @@ namespace log4net.Extensions.AspNetCore
 {
     public static class log4netExtensions
     {
-        public static void AddLog4Net(this ILoggerFactory loggerFactory, bool enableScopes = false)
+
+#if NETSTANDARD2_0
+		public static ILoggingBuilder AddLog4Net(this ILoggingBuilder loggingBuilder) {
+			return loggingBuilder.AddLog4Net(false);
+		}
+		public static ILoggingBuilder AddLog4Net(this ILoggingBuilder loggingBuilder, bool enableScopes) {
+			loggingBuilder.AddProvider(new log4netLogProvider(enableScopes));
+			return loggingBuilder;
+		}
+		public static ILoggingBuilder AddLog4Net(this ILoggingBuilder loggingBuilder, string configurationPath) {
+			return loggingBuilder.AddLog4Net(false, configurationPath, Assembly.GetEntryAssembly());
+		}
+		public static ILoggingBuilder AddLog4Net(this ILoggingBuilder loggingBuilder, string configurationPath, Assembly repositoryAssembly) {
+			return loggingBuilder.AddLog4Net(false, configurationPath, repositoryAssembly);
+		}
+		public static ILoggingBuilder AddLog4Net(this ILoggingBuilder loggingBuilder, bool enableScopes, string configurationPath, Assembly repositoryAssembly) {
+			loggingBuilder.AddLog4Net(enableScopes);
+			loggingBuilder.ConfigureLog4Net(configurationPath, repositoryAssembly);
+			return loggingBuilder;
+		}
+
+		public static ILoggingBuilder ConfigureLog4Net(this ILoggingBuilder loggingBuilder, string configurationPath) {
+			loggingBuilder.ConfigureLog4Net(configurationPath, Assembly.GetEntryAssembly());
+			return loggingBuilder;
+		}
+
+		public static ILoggingBuilder ConfigureLog4Net(this ILoggingBuilder loggingBuilder, string configurationPath, Assembly repositoryAssembly) {
+			var logRepository = LogManager.GetRepository(repositoryAssembly);
+			XmlConfigurator.ConfigureAndWatch(logRepository, new FileInfo(configurationPath));
+			return loggingBuilder;
+		}
+#endif
+
+		public static void AddLog4Net(this ILoggerFactory loggerFactory, bool enableScopes = false)
         {
             loggerFactory.AddProvider(new log4netLogProvider(enableScopes: enableScopes));
         }
